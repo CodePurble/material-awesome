72b4c481bbb0ff3ca486330ede02064f07886ba3
CodePurble <ramprakash.chandrashekar@gmail.com>
2019-12-10 00:33:08 +0530

    Added firefox icon

--- configuration/tags/init.lua
+++ configuration/tags/init.lua
@@ -11,8 +11,8 @@ local tags = {
     screen = 1
   },
   {
-    icon = icons.chrome,
-    type = 'chrome',
+    icon = icons.firefox,
+    type = 'firefox',
     defaultApp = apps.default.browser,
     screen = 1
   },
--- theme/icons/init.lua
+++ theme/icons/init.lua
@@ -3,6 +3,7 @@ local dir = os.getenv('HOME') .. '/.config/awesome/theme/icons'
 return {
   --tags
   chrome = dir .. '/google-chrome.svg',
+  firefox = dir .. '/icons8-firefox.svg',
   code = dir .. '/code-braces.svg',
   social = dir .. '/forum.svg',
   folder = dir .. '/folder.svg',
